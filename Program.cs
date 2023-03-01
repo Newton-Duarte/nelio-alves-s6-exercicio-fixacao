1d601dd35e766af7750db99e1e865609193e3ff5
Newton Duarte Cavalcanti <newton_@hotmail.com>
2023-02-28 21:04:54 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace ExercicioFixacao
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Student[] students = new Student[10];
+
+      System.Console.Write("How many rooms will be rented? ");
+      int rooms = int.Parse(Console.ReadLine());
+
+      for (int i = 0; i < rooms; i++)
+      {
+        System.Console.WriteLine("Rent #" + (i + 1));
+
+        System.Console.Write("Name: ");
+        string studentName = Console.ReadLine();
+
+        System.Console.Write("Email: ");
+        string studentEmail = Console.ReadLine();
+
+        System.Console.Write("Room: ");
+        int studentRoom = int.Parse(Console.ReadLine());
+
+        students[studentRoom] = new Student {
+          Name = studentName,
+          Email = studentEmail
+        };
+      }
+
+      System.Console.WriteLine("Busy rooms:");
+      for (int i = 0; i < students.Length; i++)
+      {
+        if (students[i] != null)
+        {
+          System.Console.WriteLine($"{i}: {students[i].Name}, {students[i].Email}");
+        }
+      }
+    }
+  }
+}
---
+++ Student.cs
@@ -0,0 +1,5 @@
+class Student
+{
+  public string Name;
+  public string Email;
+}
